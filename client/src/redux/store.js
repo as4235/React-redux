4c61718e3709d8f5791d874b9f78d5698aa3d4e5
as4235 <as4235@njit.edu>
2021-09-01 10:28:25 -0400

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit"; /* stores state, combine reducers */
-import todoReducer from "./todoSlice";
+import todoReducer from "./todoSlice"; /* Importing the reducer in todoSlice */
 
 /* link the store to the app */
 export default configureStore({
